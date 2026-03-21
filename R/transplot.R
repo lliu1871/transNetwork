10297888f1aa0672c787dda3dbaf5b72b2f5ff56
lliu1871 <lliu@uga.edu>
2026-03-21 14:18:25 -0400

    Update functions and documentation

--- R/transplot.R
+++ R/transplot.R
@@ -21,42 +21,51 @@
 #' @export
 
 transplot <- function(transmission, style = 1, vcolor = "lightblue", vsize = 12, label.cex = 1.5, showlabel = FALSE, dateLastSample = 2008) {
-  # Filter out NA infectors and edges with prob<0.1
   edges_plot <- transmission
-  edges_plot <- edges_plot[!is.na(edges_plot$infector),]
-  edges_plot <- edges_plot[which(edges_plot$prob>0.1),]
   
-  # Create the graph first
-  g <- graph_from_data_frame(edges_plot[, c("infector", "infectee")], directed = TRUE)
-  V(g)$name <- as.character(V(g)$name)
-  
-  # Use vcount(g) to get the correct number of unique individuals
-  n_unique_vertices <- vcount(g)
-  
-  # Initialize attributes using the correct length
-  vertex_colors    <- rep(vcolor, n_unique_vertices)
-  vertex_sizes     <- rep(vsize, n_unique_vertices)
-  vertex_label_cex <- rep(label.cex, n_unique_vertices)
-  
-  # Find the root of the network
-  root_name <- V(g)$name[is.na(match(V(g)$name,as.character(edges_plot$infectee)))]
-  target_idx <- which(V(g)$name == root_name)
-  if (length(root_name) == 1) {
-    vertex_colors[target_idx] <- "grey"
-    vertex_sizes[target_idx]  <- vertex_sizes[target_idx] + 2
-    vertex_label_cex[target_idx] <- vertex_label_cex[target_idx] + 0.2
+  if(style==1){
+    # Filter out NA infectors and edges with prob<0.1
+    edges_plot <- edges_plot[!is.na(edges_plot$infector),]
+    edges_plot <- edges_plot[which(edges_plot$prob>0.1),]
+  }else if(style==2){
+    # convert NA infectors (external) to "source"
+    edges_plot$infector[is.na(edges_plot$infector)] <- "source"
+    edges_plot$infector[which(edges_plot$prob<0.1)] <- "source"
   }
-  
-  # add color and label
-  E(g)$color <- ifelse(edges_plot$prob < 0.5, "pink",
-                       ifelse(edges_plot$prob < 0.75, "#a1a112", "#6b1a1a")
-  )
-  if (showlabel) {
-    E(g)$label <- as.character(round(edges_plot$prob,2))
-  } else {
-    E(g)$label <- ""
+    
+  if(style==1 | style==2){
+    # Create the graph first
+    g <- graph_from_data_frame(edges_plot[, c("infector", "infectee")], directed = TRUE)
+    V(g)$name <- as.character(V(g)$name)
+    
+    # Use vcount(g) to get the correct number of unique individuals
+    n_unique_vertices <- vcount(g)
+    
+    # Initialize attributes using the correct length
+    vertex_colors    <- rep(vcolor, n_unique_vertices)
+    vertex_sizes     <- rep(vsize, n_unique_vertices)
+    vertex_label_cex <- rep(label.cex, n_unique_vertices)
+    
+    # Find the root of the network
+    root_name <- V(g)$name[is.na(match(V(g)$name,as.character(edges_plot$infectee)))][1]
+    target_idx <- which(V(g)$name == root_name)
+    if (length(root_name) == 1) {
+      vertex_colors[target_idx] <- "grey"
+      vertex_sizes[target_idx]  <- vertex_sizes[target_idx] + 2
+      vertex_label_cex[target_idx] <- vertex_label_cex[target_idx] + 0.2
+    }
+    
+    # add color and label
+    E(g)$color <- ifelse(edges_plot$prob < 0.5, "pink",
+                         ifelse(edges_plot$prob < 0.75, "#a1a112", "#6b1a1a")
+    )
+    if (showlabel) {
+      E(g)$label <- as.character(round(edges_plot$prob,2))
+    } else {
+      E(g)$label <- ""
+    }
   }
-  
+
   if (style == 1) { # network style plot
     plot(
       g,
@@ -70,7 +79,7 @@ transplot <- function(transmission, style = 1, vcolor = "lightblue", vsize = 12,
     )
     legend("topright", legend = c("< 0.5", "0.5 - 0.75", "> 0.75"), col = c("pink", "#a1a112", "#6b1a1a"), pch = 19, bty = "n", cex = 1.0)
   } else if (style == 2) { # rooted-tree-style plot
-    layout <- layout_as_tree(g, root = which(V(g)$name == root_name))
+    layout <- layout_as_tree(g, root = which(V(g)$name == "source"))
     plot(
       g,
       layout = layout,
